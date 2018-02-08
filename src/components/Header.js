1a07f446103d8ee4d994bf0ea02c65b9079f4ea5
haidardzaky <haidardz@gmail.com>
2018-02-08 15:09:43 +0700

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import logo from './NBA_AllStar_LA.png'
+import "./Component.css"
+
+const Header = () => (
+  <div className="App-header">
+    <img src={logo} className="App-logo" alt="logo"/>
+    <h1 className="App-title">Welcome to React</h1>
+  </div>
+  )
+
+export default Header
