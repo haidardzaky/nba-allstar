9a9cd94c4c93246253da6e5a55668fe632d3eb3a
haidardzaky <haidardz@gmail.com>
2018-02-08 17:52:53 +0700

    add backcourt option

--- src/components/Backcourt.js
+++ src/components/Backcourt.js
@@ -8,6 +8,27 @@ export default class Backcourt extends Component {
       <div class="ui card">
         <Profile image="/images/kyrieirving.png" name="Kyrie Irving" team="Boston Celtics" number="191 cm" trophy="1x NBA Champion" star="5x NBA All-Star"/>
       </div>
+      <div class="card">
+        <Profile image="/images/stephencurry.png" name="Stephen Curry" team="Golden State Warriors" number="190 cm" trophy="2x NBA Champions" star="5x NBA All-Star"/>
+      </div>
+      <div class="card">
+        <Profile image="/images/rickyrubio.png" name="Ricky Rubio" team="Utah Jazz" number="193 cm" trophy="-" star="-"/>
+      </div>
+      <div class="card">
+        <Profile image="/images/jamesharden.png" name="James Harden" team="Houston Rockets" number="196 cm" trophy="-" star="6x NBA All-Star"/>
+      </div>
+      <div class="card">
+        <Profile image="/images/isaiahthomas.png" name="Isaiah Thomas" team="Cleveland Cavaliers" number="175 cm" trophy="-" star="2x NBA All-Star"/>
+      </div>
+      <div class="card">
+        <Profile image="/images/tonyparker.png" name="Tony Parker" team="San Antonio Spurs" number="188 cm" trophy="4x NBA Champions" star="6x NBA All-Star"/>
+      </div>
+      <div class="card">
+        <Profile image="/images/dwyanewade.png" name="Dwyane Wade" team="Cleveland Cavaliers" number="194 cm" trophy="3x NBA Champions" star="12x NBA All-Star"/>
+      </div>
+      <div class="card">
+        <Profile image="/images/westbrook.png" name="Russell Westbrook" team="Oklahoma City Thunder" number="191 cm" trophy="-" star="7x NBA All-Star"/>
+      </div>
     </div>)
   }
 }
